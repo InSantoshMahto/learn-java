f9d4fec18e01ae511a5cf58f19eaf9cecff5017e
Santosh Mahto <smahto@vertel.com.au>
2023-04-04 18:11:33 +0530

    structure created for three sum

--- LeetCode.java
+++ LeetCode.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class LeetCode {
   public static void main(String[] args) {
     LeetCode lc = new LeetCode();
-    int[] arrayOfNumbers = { 2, 7, 11, 15 };
-    int target = 9;
-    int[] result = lc.twoSum(arrayOfNumbers, target);
+    
+    // https://leetcode.com/problems/two-sum/
+    int[] arrayOfNumbersForTwoSum = { 2, 7, 11, 15 };
+    int targetForTwoSum = 9;
+    int[] resultForTwoSum = lc.twoSum(arrayOfNumbersForTwoSum, targetForTwoSum);
     System.out.println("Two Sum of:-");
-    lc.printArray(result);
+    lc.printArray(resultForTwoSum);
+    
+    // https://leetcode.com/problems/two-sum/
+    int[] arrayOfNumbersForThreeSum = {-1,0,1,2,-1,-4};
+    List<List<Integer>> resultForThreeSum = lc.threeSum(arrayOfNumbersForThreeSum);
+    System.out.println("Three Sum of:-");
+    System.out.println(resultForThreeSum);
   }
 
   private int[] twoSum(int[] numbers, int target) {
@@ -24,6 +35,29 @@ public class LeetCode {
       map.put(number, i);
     }
     return result;
+  }
+
+
+  private  List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> results =  new ArrayList<List<Integer>>();
+    if(nums.length < 3) return results;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i = 0; i < nums.length - 2; i++){
+      for(int j = i + 1; j < nums.length - 1; j++ ) {
+        for(int k = j + 1; k < nums.length; k++){
+          if(nums[i] + nums[j] + nums[k] == 0) {
+            List<Integer> result = new ArrayList<Integer>();
+            result.add(nums[i]);
+            result.add(nums[j]);
+            result.add(nums[k]);
+            Collections.sort(result);
+            if(!results.contains(result))
+              results.add(result);
+          }
+        }
+      }
+    }
+    return results;
   }
 
   private void printArray(int[] numbers) {
