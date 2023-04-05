9560bdf055b79159d6d74885d0fd584c6a2df09c
Santosh Mahto <smahto@vertel.com.au>
2023-04-05 17:01:46 +0530

    learn recursion

--- practice/Day24.java
+++ practice/Day24.java
@@ -6,14 +6,55 @@ public class Day24 {
 
     int n = 10;
     System.out.print(n + " to 1 recursively:- ");
-    printAto1(n);
+    printNto1(n);
+    System.out.println();
+
+    System.out.print("1 to " + n + " recursively:- ");
+    print1toN(n);
+    System.out.println();
+
+    int fact = factorial(n);
+    System.out.println("Factorial of " + n + " recursively:- " + fact);
+
+    int fib = fibonacci(3);
+    System.out.println("Fibonacci of " + n + " recursively:- " + fib);
+
+    System.out.println("madam is Palindrome:- " + isPalindrome("madam"));
   }
 
-  public static void printAto1(int A) {
+  public static void printNto1(int A) {
     System.out.print(A + " ");
     if (A > 1)
-      printAto1(A - 1);
+      printNto1(A - 1);
     else
       System.out.println("");
+  }
+
+  public static void print1toN(int A) {
+    if (A > 0) {
+      print1toN(A - 1);
+      System.out.print(A + " ");
+    }
+  }
+
+  public static int factorial(int A) {
+    return A == 1 ? A : A * factorial(A - 1);
+  }
+
+  public static int fibonacci(int A) {
+    return A == 0 || A == 1 ? A : fibonacci(A - 1) + fibonacci(A - 2);
+  }
+
+  public static boolean isPalindrome(String str, int left, int right) {
+    if (left >= right)
+      return true;
+    return str.charAt(left) == str.charAt(right) && isPalindrome(str, left + 1, right - 1);
+  }
+
+  public static boolean isPalindrome(String str) {
+    int left = 0, right = str.length() - 1;
+    if (left >= right)
+      return true;
+    return str.charAt(left) == str.charAt(right) && isPalindrome(str, left + 1, right - 1);
   }
 }
